fcd0aa3f9effa68e4c9656a226547bdd4c2a1005
cristianbambague <criistiianb10@gmail.com>
2021-10-26 15:45:31 -0500

    terminado frontend

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Producto } from '../../models/producto';
-import { Router} from '@angular/router';
+import { ActivatedRoute, Router} from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { ProductoService } from 'src/app/services/producto.service';
+
 
 @Component({
   selector: 'app-crear-producto',
@@ -10,21 +12,26 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./crear-producto.component.css']
 })
 export class CrearProductoComponent implements OnInit {
-  
-  productoForm: FormGroup;
 
+  productoForm: FormGroup;
+  titulo = 'Crear Producto'
+  id: string | null;
   constructor(private fb: FormBuilder,
               private router: Router,
-              private toastr: ToastrService) {
+              private toastr: ToastrService,
+              private _productService: ProductoService,
+              private aRouter: ActivatedRoute) {
     this.productoForm = this.fb.group({
       producto:  ['', Validators.required],
       categoria: ['', Validators.required],
       ubicacion: ['', Validators.required],
       precio: ['', Validators.required],
     });
+    this.id = this.aRouter.snapshot.paramMap.get('id');
   }
 
   ngOnInit(): void {
+    this.esEditar();
   }
 
   agregarProducto(){
@@ -36,10 +43,43 @@ export class CrearProductoComponent implements OnInit {
       precio: this.productoForm.get('precio')?.value
     }
 
-    console.log( productoObj );
-    this.toastr.success('Producto registrado con exito', 'Producto Registrado');
-    this.router.navigate(['/'])
-    
+    if (this.id !== null) {
+
+      this._productService.editarPorducto(this.id, productoObj).subscribe(data =>{
+        this.toastr.info('Producto actualizado con exito', 'Producto actulizado.');
+        this.router.navigate(['/'])
+      }, error =>{
+        console.log(error);
+        this.toastr.error('Error', 'Al actualizar el producto.');
+        this.productoForm.reset();
+      })
+
+    }else{
+
+      this._productService.guardarProducto(productoObj).subscribe(data =>{
+        this.toastr.success('Producto registrado con exito', 'Producto Registrado');
+        this.router.navigate(['/'])
+      }, error =>{
+        console.log(error);
+        this.toastr.error('Error', 'Al registrar el producto.');
+        this.productoForm.reset();
+      });
+
+    }
+  }
+
+  esEditar(){
+    if (this.id !== null) {
+      this.titulo = 'Editar Producto';
+      this._productService.obtenerProducto(this.id).subscribe(data =>{
+        this.productoForm.setValue({
+          producto:  data.nombre,
+          categoria: data.categoria,
+          ubicacion: data.ubicacion,
+          precio: data.precio,
+        })
+      })
+    }
   }
 
 }
--- src/app/components/listar-productos/listar-productos.component.ts
+++ src/app/components/listar-productos/listar-productos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+import { ProductoService } from '../../services/producto.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-listar-productos',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarProductosComponent implements OnInit {
 
-  constructor() { }
+  listarProductos: Producto[] = [];
+
+  constructor(private _productoService: ProductoService,
+              private toastr: ToastrService ) { }
 
   ngOnInit(): void {
+    this.obtenerProducto();
+  }
+
+  obtenerProducto(){
+
+    this._productoService.getProductos().subscribe(data =>{
+      console.log(data);
+      this.listarProductos = data;
+    }, error =>{
+      console.log(error);
+    })
+  }
+
+  eliminarProducto(id: any){
+    this._productoService.eliminarProducto(id).subscribe(data =>{
+      this.toastr.error('El producto fue eliminado','El producto ');
+      this.obtenerProducto();
+    }, error =>{
+      console.log(error);
+      this.toastr.error('Error','Error al eliminar');
+    });
   }
 
 }
